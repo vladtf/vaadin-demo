420e95df0887f9bd6d986276f74f37dcf3b1760d
vladtf <vendeta0263@gmail.com>
2020-03-08 00:28:18 +0200

    Add grid to SandBoxView to display data from database

--- src/main/java/com/live/vladislav/ui/views/SandBoxView.java
+++ src/main/java/com/live/vladislav/ui/views/SandBoxView.java
@@ -1,7 +1,11 @@
 package com.live.vladislav.ui.views;
 
+import com.live.vladislav.backend.entity.Company;
+import com.live.vladislav.backend.entity.Contact;
+import com.live.vladislav.backend.service.ContactService;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.annotation.UIScope;
@@ -9,7 +13,38 @@ import com.vaadin.flow.spring.annotation.UIScope;
 @Route(value = "sandbox")
 @UIScope
 public class SandBoxView extends VerticalLayout {
-    public SandBoxView() {
+
+    Grid<Contact> grid = new Grid<>(Contact.class);
+    private ContactService contactService;
+
+    public SandBoxView(ContactService contactService) {
+        this.contactService = contactService;
         add(new Button("Go back", buttonClickEvent -> UI.getCurrent().navigate("root")));
+
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+
+        add(grid);
+        updateList();
+    }
+
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.removeColumnByKey("company");
+        grid.setColumns("firstName", "lastName", "email", "status", "company");
+        grid.addColumn(contact -> {
+            Company company = contact.getCompany();
+            return company == null ? "-" : company.getName();
+        }).setHeader("Company");
+
+        grid.getColumns().forEach(col-> {
+            col.setAutoWidth(true);
+        });
+    }
+
+    private void updateList() {
+        grid.setItems(contactService.findAll());
     }
 }
