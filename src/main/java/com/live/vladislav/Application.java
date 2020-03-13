a1d6cd0eea458111654d4856c6034c8b8aa171aa
vladtf <vendeta0263@gmail.com>
2020-03-13 11:47:08 +0200

    Add a login page and configured security with Spring Security

--- src/main/java/com/live/vladislav/Application.java
+++ src/main/java/com/live/vladislav/Application.java
@@ -1,16 +1,14 @@
 package com.live.vladislav;
 
-import com.vaadin.flow.server.VaadinRequest;
-import com.vaadin.flow.server.osgi.VaadinBundleTracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.format.datetime.standard.Jsr310DateTimeFormatAnnotationFormatterFactory;
 
 /**
  * The entry point of the Spring Boot application.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
